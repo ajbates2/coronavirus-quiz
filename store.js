04a45578b252f66a202bcc243bdd24fb961b1006
ajbates2 <60674015+ajbates2@users.noreply.github.com>
2020-03-19 19:49:10 -0500

    first wireframe attempt

---
+++ store.js
@@ -0,0 +1,57 @@
+const STORE = [
+    {//1
+    question: 'Which of these is NOT a strain of Coronavirus?',
+    options: ['SARS', 'MERS', 'T-VIRUS', 'COVID=19'],
+    answer: 'T-VIRUS'
+    },
+    {//2
+    question: 'What does "Corona" mean in latin?',
+    options: ['Beer', 'Crown', 'Tyrant', 'Hat'],
+    answer: 'Crown'
+    },
+    {//3
+    question: 'Coronavirus typicaly affects which part of the human body?',
+    options: ['Upper respiratory system', 'Intestinal tract', 'Sinuses', 'The brain'],
+    answer: 'Upper respiratory system'
+    },
+    {//4
+    question: 'Which of these animals have we NOT found to carry a strain of Coronavirus',
+    options: ['Alpacas', 'Bats', 'Pigs', 'Alligators'],
+    answer: 'Alligators'
+    },
+    {//5
+    question: 'COVID-19 is currently thought to have originated from which of these animals?',
+    options: ['Alpacas', 'Bats', 'Pigs', 'Alligators'],
+    answer: 'Bats'
+    },
+    {//6
+    question: 'Covid-19 is though to have originated from which region?',
+    options: ['Raccoon City', 'Cairo, Egypt', 'Wuhan, China', 'Venice, Italy'],
+    answer: "Wuhan, China"
+    },
+    {//7
+    question: 'Which of these things should you NOT be doing during the current COVID-19 outbreak?',
+    options: ['Wash your hands', 'Social Distancing', 
+    'Travel to other countries', 'Buy mass quantities of toilet paper'],
+    answer: 'Travel to other countries'
+    },
+    {//8
+    question: 'Who is the most at risk during this time?',
+    options: ['The elderly', 'People with auto-immune disorders', 
+    'People with pre-existing respiratory disorders', 'All of the above'],
+    answer: 'All of the above'
+    },
+    {//9
+    question: 'Why is it important to practice social distancing at this time?',
+    options: ['To keep those at risk from catching COVID-19',
+     'To finish off your backlog of video games', 
+     'To help sread COVID-19', 
+     'To keep Umbrella Corp from running tests on you'],
+    answer: 'To keep those at risk from catching COVID-19'
+    },
+    {//10
+    question: 'Is there a cure for COVID-19 at this time',
+    options: ['No', 'Yes'],
+    answer: 'No'
+    },
+]
