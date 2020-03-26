c3953b675700283cf6eebd0a2014aaea26893255
ajbates2 <60674015+ajbates2@users.noreply.github.com>
2020-03-26 12:53:54 -0500

    completed for now

--- index.js
+++ index.js
@@ -1,69 +1,119 @@
-let questionNumber = 0;
+let questionIndex = 0;
 let score = 0;
-let question = STORE.questions[questionNumber];
+
+function showQuestion(questionIndex) {
+    $('#js-questions').html(`
+    <h2 class="question js-question">${STORE.questions[questionIndex].question}</h2>
+    <fieldset class="container js-container">
+        <input type="radio" name="options" id="ans1" value="0" required>
+            <label for="ans1">${STORE.questions[questionIndex].options[0]}</label>
+            <br>
+        <input type="radio" name="options" id="ans2" value="1">
+            <label for="ans2">${STORE.questions[questionIndex].options[1]}</label>
+            <br>
+        <input type="radio" name="options" id="ans3" value="2">
+            <label for="ans3">${STORE.questions[questionIndex].options[2]}</label>
+            <br>
+        <input type="radio" name="options" id="ans4" value="3">
+            <label for="ans4">${STORE.questions[questionIndex].options[3]}</label>
+    </fieldset>
+    <p class="score">score: <span id="js-score">${score}</span></p>
+    <p class="js-validation"></p>
+    <div class="button-list">
+        <p id="button"><button type="submit" for="js-questions" id="js-submit">Submit</button></p>
+        <p id="js-next-question"><button type="button" id="next-question">Next Question</button></p>
+    </div>`)
+}
 
 function updateQuestionNumber() {
-    $('.js-questionNumber').text(questionNumber + 1);
+    $('.js-questionNumber').text(questionIndex + 1);
 }
 
-function updateQuestion() {
-    $('.js-question').text(`${question.question}`);
-}
 
-function updateIndex(item) {
-    for(let i = 0; i < item; i++) {
-        item = i++;
-    }
-}
-
-function updateOptions() {
-    $('.js-ans1').text(`${question.options[0]}`);
-    $('.js-ans2').text(`${question.options[1]}`);
-    $('.js-ans3').text(`${question.options[2]}`);
-    $('.js-ans4').text(`${question.options[3]}`);
+function updateNextQuestion() {
+    $('#js-next-question').on('click', '#next-question', function(event) {
+        console.log(this);
+        questionIndex++;
+        console.log(questionIndex);       
+        showQuestion(questionIndex);
+        updateQuestionNumber();
+    })
 }
 
 function updateScore() {
-    $('.js-score').text('score: ' + score);
+    $('#js-score').text(score);
 }
 
 function generateAnswerValidation() {
-    $('.js-container').one('click', 'label', function(event) {
-        let targetOption = $(this).text();
-        $('label').val(targetOption);
-        console.log(targetOption);
-        if (question.answer === targetOption) {
-           $('.js-validation').text('That is Correct!');
-           updateIndex(score);
-        }
-        else if (question.answer !== event.currentTarget) {
-            $('.js-validation').text(`That is incorrect. The right answer is ${question.answer}`)
-        }
-        generateNextQuestionButton();
-        updateScore();
-    });
-}
-
-function generateNextQuestionButton() {
- $('.js-button').html('<button type="submit" for="js-questions" id="js-next-question">Next Question</button>')
-}
-
-function nextQuestion() {
-    $('main').on('submit', '.question-form', function(event) {
+    $('#js-questions').on('submit', function(event) {
         event.preventDefault();
-        console.log(this);
-        updateIndex(questionNumber);
-        console.log(questionNumber);
+        let answerValidation = STORE.questions[questionIndex];
+        let targetOption = $("input[name='options']:checked").val()
+        if (answerValidation.answer == targetOption) {
+           $('.js-validation').text('That is Correct!');
+           score++;
+        }
+        else {
+            $('.js-validation').text(`That is incorrect. The right answer is 
+            ${answerValidation.options[answerValidation.answer]}`);
+            console.log(answerValidation.options[answerValidation.answer])
+        }
+        updateNextQuestion();
+        renderResults();
     });
-};
+}
+
+function showResults() {
+    $('body').html(`
+    <header>
+        <h1>Results</h1>
+    </header>
+    <main>
+        <section>
+            <h2 id="result-conditional"></h2>
+                <p class="final-score">${score}/10</p>
+        </section>  
+        <button onclick="document.location = 'index.html'">Restart Quiz</button>
+        <aside class="final-aside">In all seriousness, PLEASE stay home, wash your hands
+             for at least 20 seconds multiple times a day, avoid touching
+            your face (even though it’s very difficult), and we might be 
+            able to keep our loved ones safe.
+        </aside> 
+    </main>`)
+}
+
+function resultConditional() {
+    $('#result-conditional').html(function() {
+        if (score >= 7) {
+            return (`You passed! You were able to decipher 
+            the difference between real life and Resident Evil!`)
+        }
+        else {
+            return (`You should research COVID-19 a little more.
+             We are not living in an Umbrella Corp run society.
+              Raccoon city isn’t real. The T-Virus has ‘not’ been created yet.
+               Zombie alligators are (hopefully) not patrolling the sewers.`)
+        }
+    })
+}
+
+function renderResults() {
+    $('body').on('click', '#next-question', function(event) {
+        if (questionIndex === STORE.questions.length) {
+            showResults();
+            resultConditional();
+        }
+        else {
+            showQuestion();
+        }
+    });
+}
 
 function handleQuiz() {
-    updateOptions();
-    updateQuestion();
     updateScore();
     updateQuestionNumber();
-    generateAnswerValidation();
-    nextQuestion();
+    generateAnswerValidation(questionIndex);
+    showQuestion(questionIndex);
 }
 
-handleQuiz();
+$(handleQuiz);
